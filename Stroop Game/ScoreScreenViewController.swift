c95b5082a930c87ae5ce3ea86a9d7741756f4576
Eric Snow <ericsnow@MediaTechs-iMac.local>
2018-12-07 13:18:18 -0600

    completed auto-layout work.

--- Stroop Game/ScoreScreenViewController.swift
+++ Stroop Game/ScoreScreenViewController.swift
@@ -22,7 +22,8 @@ class ScoreScreenViewController: UIViewController {
     var updateGameScore = 0
     var congratsHighScore = false
     private var blinkTimer: Timer?
-    private var blinkColorsArray = [#colorLiteral(red: 0.5981579423, green: 0.1302183867, blue: 0.07905782014, alpha: 1), #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), #colorLiteral(red: 0.7728164792, green: 0.6411972642, blue: 0.2131647766, alpha: 1), #colorLiteral(red: 0.1021426842, green: 0.1914339662, blue: 0.07176273316, alpha: 1)]
+    private var blinkColorsArray = [#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1), #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1), #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1), #colorLiteral(red: 0, green: 0.5603182912, blue: 0, alpha: 1), #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)]
+    private var loserColorsArray = [#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)]
 
     
     ///Updates score labels to score screen.
@@ -36,20 +37,25 @@ class ScoreScreenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         postScores()
+
     }
     
-    
+    override func viewWillAppear(_ animated: Bool) {
+    }
     
 ///Cycles through array of colors to blink the label if new high score is achieved.
     private func colorTimer() {
         var myColorInt = 0
         blinkTimer = Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { timer in
             myColorInt += 1
-            myColorInt %= 4
+            myColorInt %= 5
             if self.congratsHighScore == true {
                 self.congratsLabel.textColor = self.blinkColorsArray[myColorInt]
-                self.congratsLabel.text = "New High Score!" +
-                "\n☃️🎁🎄🎅🎄🎁☃️"
+                self.congratsLabel.text = "new\nhigh\nscore" 
+            } else {
+                self.congratsLabel.textColor = self.loserColorsArray[myColorInt]
+                self.congratsLabel.text = "you\nget\ncoal"
+
             }
         }
     }
